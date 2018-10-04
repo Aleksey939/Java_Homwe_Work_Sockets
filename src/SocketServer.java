bf2083515b5ae8ae08a162ab47ca4c8dfccb5da2
Aleksey939 <Aleksey93919@gmail.com>
2018-10-04 19:42:46 +0300

    ExecutorService

--- src/SocketServer.java
+++ src/SocketServer.java
@@ -1,55 +1,54 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-public class SocketServer  extends Thread {
-    static final int PORT = 10000;
-    static final String HOST = "localhost";
-    private  boolean connected;
+public class SocketServer  {
 
-    Socket remoteClient = null;
-       public SocketServer(Socket rc,boolean connected ){
-        this.remoteClient=rc;
-        this.connected=connected;
-    }
-    public void run()  {
-        System.out.print("Connected to" + remoteClient.getRemoteSocketAddress());
-        while (connected) {
-            try {
-                InputStream in = remoteClient.getInputStream();
-                InputStreamReader r = new InputStreamReader(in);
-                BufferedReader reader = new BufferedReader(r);
-                OutputStream out = remoteClient.getOutputStream();
-                PrintStream print = new PrintStream(out, true);
+    final static int PORT = 10000;
+    final String HOST = "localhost";
+        public static void main(String arg[]) throws IOException {
 
-                String msg = reader.readLine();
-                System.out.print("Server: Client send msg" + msg);
-                print.println(msg);
-                if (msg.contains("Bye"))
-                    connected = false;
-            }
-            catch (Exception ex) {
-                ex.printStackTrace();
-            }
-        }
-        try {
-            remoteClient.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void main(String arg[]) throws IOException {
         ServerSocket server = new ServerSocket(PORT);
         System.out.print("Wait for Connection...");
-     boolean connected = true;
-       Socket remoteClient = null;
+        final boolean[] connected = {true};
+        Socket remoteClient = null;
+        ExecutorService service = Executors.newCachedThreadPool();
         while (true) {
             remoteClient = server.accept();
-            new SocketServer(remoteClient,connected).start();
+            System.out.print("Connected to" + remoteClient.getRemoteSocketAddress());
+            Socket finalRemoteClient = remoteClient;
+            service.submit(new Runnable() {
+                public void run() {
+                    while (connected[0]) {
+                        try {
+                            InputStream in = finalRemoteClient.getInputStream();
+                            InputStreamReader r = new InputStreamReader(in);
+                            BufferedReader reader = new BufferedReader(r);
+                            OutputStream out = finalRemoteClient.getOutputStream();
+                            PrintStream print = new PrintStream(out, true);
+
+                            String msg = reader.readLine();
+                            System.out.print("Server: Client send msg" + msg);
+                            print.println(msg);
+                            if (msg.contains("Bye"))
+                                connected[0] = false;
+                        } catch (Exception ex) {
+                            ex.printStackTrace();
+                        }
+                    }
+                    try {
+                        finalRemoteClient.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+
+            });
 
         }
-
     }
 }
 
